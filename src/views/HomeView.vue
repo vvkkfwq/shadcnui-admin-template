109d0edf848bc7b95d89ecc0551916a47ee76a4a
Vickey Feng <vvkkfwq@sina.com>
2024-11-13 20:08:09 +0800

    style(HomeView): style adjustments

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,12 +4,12 @@ import ThemeButton from '../components/ThemeButton.vue'
 
 <template>
   <header class="sticky top-0 bg-background/80 backdrop-blur-lg border-b border-border">
-    <div class="container flex justify-between h-14 items-center">
+    <div class="flex justify-between h-14 px-10 items-center">
       <div class="flex items-center font-bold">Template System</div>
       <ThemeButton></ThemeButton>
     </div>
   </header>
   <main>
-    <div class="container pt-4 flex items-center justify-center">Welcome to home page</div>
+    <div class="pt-4 flex items-center justify-center">Welcome to home page</div>
   </main>
 </template>
