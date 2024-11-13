f07ac467645e37a0ae7ff9b7bff4c599f6c529d0
Vickey Feng <vvkkfwq@sina.com>
2024-11-13 18:59:04 +0800

    chore: cleanup template code and set up basic home page
    - Removed unnecessary template code from Vite Vue setup
    - Added required dependencies
    - Built a basic structure for the home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import ThemeButton from '../components/ThemeButton.vue'
 </script>
 
 <template>
+  <header class="sticky top-0 bg-background/80 backdrop-blur-lg border-b border-border">
+    <div class="container flex justify-between h-14 items-center">
+      <div class="flex items-center font-bold">Template System</div>
+      <ThemeButton></ThemeButton>
+    </div>
+  </header>
   <main>
-    <TheWelcome />
+    <div class="container pt-4 flex items-center justify-center">Welcome to home page</div>
   </main>
 </template>
